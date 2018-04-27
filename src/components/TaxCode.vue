856225a4ca2d96b90dffaf541c77c7ba85d1ad60
Ulisse <ulisse@MacBook-Pro-di-Ulisse.local>
2018-04-27 22:47:22 +0200

    Added sparql query to fetch list of italian districts

--- src/components/TaxCode.vue
+++ src/components/TaxCode.vue
@@ -10,68 +10,66 @@
         </nav>
       </div>
     </header> -->
-  <main role="main" class="inner cover">
-    <div class="tax-code-card">
-      <div class="header-card">
-        HEADER VERDE
-      </div>
-      <div class="content-card">
-        <form id="taxCodeForm" v-on:submit="createTaxCode">
-          <div class="form-group row">
-              <label class="col-sm-2 col-form-label" for="taxCode">CODICE FISCALE</label>
-              <div class="col-sm-10">
-                <input v-model="taxCodeOut" type="text" class="form-control" id="taxCode" placeholder="Codice Fiscale" disabled>
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-sm-2 col-form-label" for="surname">COGNOME</label>
-              <div class="col-sm-10">
-                <input v-model="surname" type="text" class="form-control" id="surname" placeholder="Cognome">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-sm-2 col-form-label" for="name">NOME</label>
-              <div class="col-sm-6">
-                <input v-model="name" type="text" class="form-control" id="name" placeholder="Nome">
-              </div>
-              <label class="col-sm-2 col-form-label" for="sex">SESSO</label>
-              <div class="col-sm-2">
-                <select v-model="sex" id="sex" class="form-control">
+<main role="main" class="inner cover">
+  <div class="tax-code-card">
+    <div class="header-card"></div>
+    <div class="content-card">
+      <form id="taxCodeForm" v-on:submit="createTaxCode">
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="taxCode">CODICE FISCALE</label>
+          <div class="col-sm-10">
+            <input v-model="taxCodeOut" type="text" class="form-control" id="taxCode" placeholder="Codice Fiscale" disabled>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="surname">COGNOME</label>
+          <div class="col-sm-10">
+            <input v-model="surname" type="text" class="form-control" id="surname" placeholder="Cognome">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="name">NOME</label>
+          <div class="col-sm-6">
+            <input v-model="name" type="text" class="form-control" id="name" placeholder="Nome">
+          </div>
+          <label class="col-sm-2 col-form-label" for="sex">SESSO</label>
+          <div class="col-sm-2">
+            <select v-model="sex" id="sex" class="form-control">
                   <option selected>M</option>
                   <option>F</option>
                 </select>
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-sm-2 col-form-label" for="birthPlace">LUOGO DI NASCITA</label>
-              <div class="col-sm-10">
-                <input v-model="birthPlace" type="text" class="form-control" id="birthPlace" placeholder="Luogo di Nascita">
-              </div>
-            </div>
-            <div class="form-group row">
-              <label class="col-sm-2 col-form-label" for="district">PROVINCIA</label>
-              <div class="col-md-3">
-                <input v-model="district" type="text" class="form-control" id="district" placeholder="Provincia">
-              </div>
-              <label class="col-sm-2 col-form-label" for="BirthData">DATA DI NASCITA</label>
-              <div class="col-md-5">
-                <datepicker></datepicker>
-                <input v-model="birthDate" type="datepicker" class="form-control" id="BirthData" placeholder="Data di nascita">
-              </div>
-            </div>
-          <button type="submit">Calcola</button>
-        </form>
-      </div>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="birthPlace">LUOGO DI NASCITA</label>
+          <div class="col-sm-10">
+            <input v-model="birthPlace" type="text" class="form-control" id="birthPlace" placeholder="Luogo di Nascita">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label" for="district">PROVINCIA</label>
+          <div class="col-md-3">
+            <input v-model="district" type="text" class="form-control" id="district" placeholder="Provincia">
+          </div>
+          <label class="col-sm-2 col-form-label" for="BirthData">DATA DI NASCITA</label>
+          <div class="col-md-5">
+            <datepicker v-model="birthDate" id="BirthDate"></datepicker>
+            <!-- <input v-model="birthDate" type="datepicker" class="form-control" id="BirthData" placeholder="Data di nascita"> -->
+          </div>
+        </div>
+        <button type="submit">Calcola</button>
+      </form>
     </div>
-  </main>
+  </div>
+</main>
 </template>
 
 <script>
-import Datepicker from 'vuejs-datepicker';
+import Datepicker from 'vuejs-datepicker'
 
 export default {
   name: 'TaxCode',
-  data () {
+  data() {
     return {
       taxCodeOut: '',
       surname: '',
@@ -79,13 +77,55 @@ export default {
       birthPlace: '',
       sex: 'M',
       district: '',
-      birthDate: ''
+      birthDate: '',
+      districts: {}
     }
   },
   methods: {
-    createTaxCode: function (event) {
-      this.taxCodeOut = `tredas ${this.birthDate}`
+    createTaxCode: function() {
+            this.taxCodeOut = `tredas ${this.birthDate}`
+    },
+    loadDistrict: function(callback) {
+      const endpointUrl = 'https://query.wikidata.org/sparql',
+        sparqlQuery = `select distinct ?comune_it ?label ?codcat {
+          ?comune_it wdt:P31 wd:Q747074.
+          ?comune_it rdfs:label ?label .
+          filter langMatches(lang(?label),"it")
+          OPTIONAL { ?comune_it wdt:P806 ?codcat. }
+        }`,
+        fullUrl = endpointUrl + '?query=' + encodeURIComponent(sparqlQuery),
+        headers = {
+          'Accept': 'application/sparql-results+json'
+        };
+
+      fetch(fullUrl, {
+        headers
+      }).then(body => body.json()).then(json => {
+        if(json) callback(json);
+        // const {
+        //   head: {
+        //     vars
+        //   },
+        //   results
+        // } = json;
+        //
+        // for (const result of results.bindings) {
+        //   for (const variable of vars) {
+        //     console.log('%s: %o', variable, result[variable])
+        //   }
+        //   console.log('---')
+        // }
+      })
     }
+  },
+  beforeMount() {
+    this.loadDistrict(output => {
+      console.log(output.results.bindings);
+      let test = output.results.bindings.filter(function(dist) {
+        return dist.label.value === 'Palermo';
+      });
+      console.log(test);
+    });
   },
   components: {
     Datepicker
@@ -99,24 +139,30 @@ h1,
 h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
+
 .cover {
   padding: 0 1.5rem;
 }
+
 .cover .btn-lg {
   padding: .75rem 1.25rem;
   font-weight: 700;
 }
+
 .tax-code-card {
   height: 400px;
   width: 628px;
@@ -126,16 +172,19 @@ a {
   color: #009247;
   text-shadow: none;
 }
+
 .header-card {
   height: 41.2%;
   background-color: #009247;
   border-radius: 25px 25px 0px 0px;
 }
+
 .content-card {
   height: 52.8%;
   font-size: 11px;
 }
-.content-card > form > .form-group {
-    margin-bottom: 0.3rem;
+
+.content-card>form>.form-group {
+  margin-bottom: 0.3rem;
 }
 </style>
