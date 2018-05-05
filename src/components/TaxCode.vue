95cd352ae83a5ecbbe618f797584d36d74514811
Ulisse <ulisse@MBP-di-Ulisse.homenet.telecomitalia.it>
2018-05-05 16:35:35 +0200

    add district with autocomplite into the view

--- src/components/TaxCode.vue
+++ src/components/TaxCode.vue
@@ -1,15 +1,4 @@
 <template>
-<!--div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-    < <header class="masthead mb-auto">
-      <div class="inner">
-        <h3 class="masthead-brand">Codice Fiscale</h3>
-        <nav class="nav nav-masthead justify-content-center">
-          <a class="nav-link active" href="#">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Contact</a>
-        </nav>
-      </div>
-    </header> -->
 <main role="main" class="inner cover">
   <div class="tax-code-card">
     <div class="header-card"></div>
@@ -32,19 +21,13 @@
         <div class="form-group row">
           <label class="col-sm-2 col-form-label" for="surname">COGNOME</label>
           <div class="col-sm-10">
-            <input v-model="surname" v-bind:class="{error: isSurError}" type="text"
-              class="form-control" id="surname" placeholder="Cognome"
-              @input="forceUpperCase('surname')"
-            >
+            <input v-model="surname" v-bind:class="{error: isSurError}" type="text" class="form-control" id="surname" placeholder="Cognome" @input="forceUpperCase('surname')">
           </div>
         </div>
         <div class="form-group row">
           <label class="col-sm-2 col-form-label" for="name">NOME</label>
           <div class="col-sm-6">
-            <input v-model="name" v-bind:class="{error: isNameError}" type="text"
-              class="form-control" id="name" placeholder="Nome"
-              @input="forceUpperCase('name')"
-            >
+            <input v-model="name" v-bind:class="{error: isNameError}" type="text" class="form-control" id="name" placeholder="Nome" @input="forceUpperCase('name')">
           </div>
           <label class="col-sm-2 col-form-label" for="sex">SESSO</label>
           <div class="col-sm-2">
@@ -56,8 +39,22 @@
         </div>
         <div class="form-group row">
           <label class="col-sm-2 col-form-label" for="birthPlace">LUOGO DI NASCITA</label>
+
           <div class="col-sm-10">
-            <input v-model="birthPlace" v-bind:class="{error: isBirthPlaceError}" type="text" class="form-control" id="birthPlace" placeholder="Luogo di Nascita">
+            <vue-instant
+            v-model="birthPlace"
+            v-bind:class="{error: isBirthPlaceError}"
+            :suggestion-attribute="suggestionAttribute"
+            :disabled="false"
+            :suggestions="districts"
+            id="birthPlace"
+            :show-autocomplete="true"
+            :autofocus="false"
+            placeholder="Luogo di Nascita"
+            type="google">
+            </vue-instant>
+
+            <!--input v-model="birthPlace" v-bind:class="{error: isBirthPlaceError}" type="text" class="form-control" id="birthPlace" placeholder="Luogo di Nascita"-->
           </div>
         </div>
         <div class="form-group row">
@@ -67,7 +64,7 @@
           </div>
           <label class="col-sm-2 col-form-label" for="BirthData">DATA DI NASCITA</label>
           <div class="col-md-5">
-            <datepicker v-model="birthDate" v-bind:class="{error: isBirthDateError}" id="BirthDate"></datepicker>
+            <datepicker v-model="birthDate" name="birthPlace" class="form-control" v-bind:class="{error: isBirthDateError}" id="BirthDate"></datepicker>
           </div>
         </div>
         <input type="submit" value="Submit">
@@ -78,6 +75,7 @@
 </template>
 
 <script>
+
 import Datepicker from 'vuejs-datepicker';
 
 export default {
@@ -96,7 +94,8 @@ export default {
       sex: 'M',
       district: '',
       birthDate: '',
-      districts: {}
+      districts: [],
+      suggestionAttribute: 'district'
     };
   },
 
@@ -153,7 +152,7 @@ export default {
       var checkBirthPlace = this.checkBirthPlace();
       this.checkDate();
 
-      if(!checkName && !checkSurname && !checkDate){
+      if (!checkName && !checkSurname && !checkDate) {
         this.createTaxCode();
       }
     },
@@ -176,7 +175,6 @@ export default {
       if (result.length < 3) {
         for (var i = 0; i < stringCheck.length; i++) {
           var isVocal = vocals.includes(stringCheck[i]);
-          // alert(this.surname[i] + ' è una vocale: ' + isVocal);
           if (isVocal) {
             if (result === undefined) {
               result = stringCheck[i];
@@ -199,13 +197,12 @@ export default {
       return result;
     },
 
-    birthDateResult: function(){
+    birthDateResult: function() {
       var result = '';
       const date = new Date(this.birthDate);
       const day = date.getDate();
       const month = date.getMonth();
       const year = date.getFullYear();
-      //console.log(`Birthate: ${day}/${month}/${year}`);
 
       var y = year.toString().substr(2);
       result += y;
@@ -214,7 +211,7 @@ export default {
       result += genericMonth[month];
 
       var d = '';
-      if(this.sex === 'M'){
+      if (this.sex === 'M') {
         day < 10 ? d = ('0' + day.toString()) : d = day.toString();
       } else if (this.sex === 'F') {
         d = (day + 40).toString();
@@ -223,109 +220,325 @@ export default {
       return result;
     },
 
-    lastCharResult: function(){
+    lastCharResult: function() {
       var evenValues = [];
       var oddValues = [];
 
-      for(var i = 0; i < this.taxCodeOut.length; i+=2){
+      for (var i = 0; i < this.taxCodeOut.length; i += 2) {
         oddValues[oddValues.length] = [this.taxCodeOut[i]];
       }
-      for(var i = 1; i < this.taxCodeOut.length; i+=2){
+      for (var i = 1; i < this.taxCodeOut.length; i += 2) {
         evenValues[evenValues.length] = [this.taxCodeOut[i]];
       }
 
       var evenValuesSchema = new Array();
-      evenValuesSchema.push({char: 0, value: 0});
-      evenValuesSchema.push({char: 1, value: 1});
-      evenValuesSchema.push({char: 2, value: 2});
-      evenValuesSchema.push({char: 3, value: 3});
-      evenValuesSchema.push({char: 4, value: 4});
-      evenValuesSchema.push({char: 5, value: 5});
-      evenValuesSchema.push({char: 6, value: 6});
-      evenValuesSchema.push({char: 7, value: 7});
-      evenValuesSchema.push({char: 8, value: 8});
-      evenValuesSchema.push({char: 9, value: 9});
-      evenValuesSchema.push({char: 'A', value: 0});
-      evenValuesSchema.push({char: 'B', value: 1});
-      evenValuesSchema.push({char: 'C', value: 2});
-      evenValuesSchema.push({char: 'D', value: 3});
-      evenValuesSchema.push({char: 'E', value: 4});
-      evenValuesSchema.push({char: 'F', value: 5});
-      evenValuesSchema.push({char: 'G', value: 6});
-      evenValuesSchema.push({char: 'H', value: 7});
-      evenValuesSchema.push({char: 'I', value: 8});
-      evenValuesSchema.push({char: 'J', value: 9});
-      evenValuesSchema.push({char: 'K', value: 10});
-      evenValuesSchema.push({char: 'L', value: 11});
-      evenValuesSchema.push({char: 'M', value: 12});
-      evenValuesSchema.push({char: 'N', value: 13});
-      evenValuesSchema.push({char: 'O', value: 14});
-      evenValuesSchema.push({char: 'P', value: 15});
-      evenValuesSchema.push({char: 'Q', value: 16});
-      evenValuesSchema.push({char: 'R', value: 17});
-      evenValuesSchema.push({char: 'S', value: 18});
-      evenValuesSchema.push({char: 'T', value: 19});
-      evenValuesSchema.push({char: 'U', value: 20});
-      evenValuesSchema.push({char: 'V', value: 21});
-      evenValuesSchema.push({char: 'W', value: 22});
-      evenValuesSchema.push({char: 'X', value: 23});
-      evenValuesSchema.push({char: 'Y', value: 24});
-      evenValuesSchema.push({char: 'Z', value: 25});
+      evenValuesSchema.push({
+        char: 0,
+        value: 0
+      });
+      evenValuesSchema.push({
+        char: 1,
+        value: 1
+      });
+      evenValuesSchema.push({
+        char: 2,
+        value: 2
+      });
+      evenValuesSchema.push({
+        char: 3,
+        value: 3
+      });
+      evenValuesSchema.push({
+        char: 4,
+        value: 4
+      });
+      evenValuesSchema.push({
+        char: 5,
+        value: 5
+      });
+      evenValuesSchema.push({
+        char: 6,
+        value: 6
+      });
+      evenValuesSchema.push({
+        char: 7,
+        value: 7
+      });
+      evenValuesSchema.push({
+        char: 8,
+        value: 8
+      });
+      evenValuesSchema.push({
+        char: 9,
+        value: 9
+      });
+      evenValuesSchema.push({
+        char: 'A',
+        value: 0
+      });
+      evenValuesSchema.push({
+        char: 'B',
+        value: 1
+      });
+      evenValuesSchema.push({
+        char: 'C',
+        value: 2
+      });
+      evenValuesSchema.push({
+        char: 'D',
+        value: 3
+      });
+      evenValuesSchema.push({
+        char: 'E',
+        value: 4
+      });
+      evenValuesSchema.push({
+        char: 'F',
+        value: 5
+      });
+      evenValuesSchema.push({
+        char: 'G',
+        value: 6
+      });
+      evenValuesSchema.push({
+        char: 'H',
+        value: 7
+      });
+      evenValuesSchema.push({
+        char: 'I',
+        value: 8
+      });
+      evenValuesSchema.push({
+        char: 'J',
+        value: 9
+      });
+      evenValuesSchema.push({
+        char: 'K',
+        value: 10
+      });
+      evenValuesSchema.push({
+        char: 'L',
+        value: 11
+      });
+      evenValuesSchema.push({
+        char: 'M',
+        value: 12
+      });
+      evenValuesSchema.push({
+        char: 'N',
+        value: 13
+      });
+      evenValuesSchema.push({
+        char: 'O',
+        value: 14
+      });
+      evenValuesSchema.push({
+        char: 'P',
+        value: 15
+      });
+      evenValuesSchema.push({
+        char: 'Q',
+        value: 16
+      });
+      evenValuesSchema.push({
+        char: 'R',
+        value: 17
+      });
+      evenValuesSchema.push({
+        char: 'S',
+        value: 18
+      });
+      evenValuesSchema.push({
+        char: 'T',
+        value: 19
+      });
+      evenValuesSchema.push({
+        char: 'U',
+        value: 20
+      });
+      evenValuesSchema.push({
+        char: 'V',
+        value: 21
+      });
+      evenValuesSchema.push({
+        char: 'W',
+        value: 22
+      });
+      evenValuesSchema.push({
+        char: 'X',
+        value: 23
+      });
+      evenValuesSchema.push({
+        char: 'Y',
+        value: 24
+      });
+      evenValuesSchema.push({
+        char: 'Z',
+        value: 25
+      });
 
       var oddValuesSchema = new Array();
-      oddValuesSchema.push({char: 0, value: 1});
-      oddValuesSchema.push({char: 1, value: 0});
-      oddValuesSchema.push({char: 2, value: 5});
-      oddValuesSchema.push({char: 3, value: 7});
-      oddValuesSchema.push({char: 4, value: 9});
-      oddValuesSchema.push({char: 5, value: 13});
-      oddValuesSchema.push({char: 6, value: 15});
-      oddValuesSchema.push({char: 7, value: 17});
-      oddValuesSchema.push({char: 8, value: 19});
-      oddValuesSchema.push({char: 9, value: 21});
-      oddValuesSchema.push({char: 'A', value: 1});
-      oddValuesSchema.push({char: 'B', value: 0});
-      oddValuesSchema.push({char: 'C', value: 5});
-      oddValuesSchema.push({char: 'D', value: 7});
-      oddValuesSchema.push({char: 'E', value: 9});
-      oddValuesSchema.push({char: 'F', value: 13});
-      oddValuesSchema.push({char: 'G', value: 15});
-      oddValuesSchema.push({char: 'H', value: 17});
-      oddValuesSchema.push({char: 'I', value: 19});
-      oddValuesSchema.push({char: 'J', value: 21});
-      oddValuesSchema.push({char: 'K', value: 2});
-      oddValuesSchema.push({char: 'L', value: 4});
-      oddValuesSchema.push({char: 'M', value: 18});
-      oddValuesSchema.push({char: 'N', value: 20});
-      oddValuesSchema.push({char: 'O', value: 11});
-      oddValuesSchema.push({char: 'P', value: 3});
-      oddValuesSchema.push({char: 'Q', value: 6});
-      oddValuesSchema.push({char: 'R', value: 8});
-      oddValuesSchema.push({char: 'S', value: 12});
-      oddValuesSchema.push({char: 'T', value: 14});
-      oddValuesSchema.push({char: 'U', value: 16});
-      oddValuesSchema.push({char: 'V', value: 10});
-      oddValuesSchema.push({char: 'W', value: 22});
-      oddValuesSchema.push({char: 'X', value: 25});
-      oddValuesSchema.push({char: 'Y', value: 24});
-      oddValuesSchema.push({char: 'Z', value: 23});
+      oddValuesSchema.push({
+        char: 0,
+        value: 1
+      });
+      oddValuesSchema.push({
+        char: 1,
+        value: 0
+      });
+      oddValuesSchema.push({
+        char: 2,
+        value: 5
+      });
+      oddValuesSchema.push({
+        char: 3,
+        value: 7
+      });
+      oddValuesSchema.push({
+        char: 4,
+        value: 9
+      });
+      oddValuesSchema.push({
+        char: 5,
+        value: 13
+      });
+      oddValuesSchema.push({
+        char: 6,
+        value: 15
+      });
+      oddValuesSchema.push({
+        char: 7,
+        value: 17
+      });
+      oddValuesSchema.push({
+        char: 8,
+        value: 19
+      });
+      oddValuesSchema.push({
+        char: 9,
+        value: 21
+      });
+      oddValuesSchema.push({
+        char: 'A',
+        value: 1
+      });
+      oddValuesSchema.push({
+        char: 'B',
+        value: 0
+      });
+      oddValuesSchema.push({
+        char: 'C',
+        value: 5
+      });
+      oddValuesSchema.push({
+        char: 'D',
+        value: 7
+      });
+      oddValuesSchema.push({
+        char: 'E',
+        value: 9
+      });
+      oddValuesSchema.push({
+        char: 'F',
+        value: 13
+      });
+      oddValuesSchema.push({
+        char: 'G',
+        value: 15
+      });
+      oddValuesSchema.push({
+        char: 'H',
+        value: 17
+      });
+      oddValuesSchema.push({
+        char: 'I',
+        value: 19
+      });
+      oddValuesSchema.push({
+        char: 'J',
+        value: 21
+      });
+      oddValuesSchema.push({
+        char: 'K',
+        value: 2
+      });
+      oddValuesSchema.push({
+        char: 'L',
+        value: 4
+      });
+      oddValuesSchema.push({
+        char: 'M',
+        value: 18
+      });
+      oddValuesSchema.push({
+        char: 'N',
+        value: 20
+      });
+      oddValuesSchema.push({
+        char: 'O',
+        value: 11
+      });
+      oddValuesSchema.push({
+        char: 'P',
+        value: 3
+      });
+      oddValuesSchema.push({
+        char: 'Q',
+        value: 6
+      });
+      oddValuesSchema.push({
+        char: 'R',
+        value: 8
+      });
+      oddValuesSchema.push({
+        char: 'S',
+        value: 12
+      });
+      oddValuesSchema.push({
+        char: 'T',
+        value: 14
+      });
+      oddValuesSchema.push({
+        char: 'U',
+        value: 16
+      });
+      oddValuesSchema.push({
+        char: 'V',
+        value: 10
+      });
+      oddValuesSchema.push({
+        char: 'W',
+        value: 22
+      });
+      oddValuesSchema.push({
+        char: 'X',
+        value: 25
+      });
+      oddValuesSchema.push({
+        char: 'Y',
+        value: 24
+      });
+      oddValuesSchema.push({
+        char: 'Z',
+        value: 23
+      });
 
       var totalResult = 0;
       var singleValue = 0;
-      for(var i = 0; i < oddValues.length; i++){
-        singleValue = oddValuesSchema.filter(function (el){
+      for (var i = 0; i < oddValues.length; i++) {
+        singleValue = oddValuesSchema.filter(function(el) {
           var value = 0
-          if(el.char == oddValues[i][0]){
+          if (el.char === oddValues[i][0]) {
             value = el.value;
             totalResult += value;
             return value;
           }
         });
       }
-      for(var i = 0; i < evenValues.length; i++){
-        singleValue = evenValuesSchema.filter(function (el){
-          var value = 0
-          if(el.char == evenValues[i][0]){
+      for (var j = 0; j < evenValues.length; j++) {
+        singleValue = evenValuesSchema.filter(function(el) {
+          var value = 0;
+          if (el.char === evenValues[j][0]) {
             value = el.value;
             totalResult += value;
             return value;
@@ -342,11 +555,11 @@ export default {
       var surnameResult = this.stringResult(this.surname);
       var nameResult = this.stringResult(this.name);
       var birthResult = this.birthDateResult(this.birthDate);
+      var lastCharResult = this.lastCharResult();
       this.taxCodeOut = surnameResult;
       this.taxCodeOut += nameResult;
       this.taxCodeOut += birthResult;
-      var lastCharResult = this.lastCharResult();
-      this.taxCodeOut +=lastCharResult;
+      this.taxCodeOut += lastCharResult;
     },
 
     loadDistrict: function(callback) {
@@ -366,9 +579,32 @@ export default {
 
       fetch(fullUrl, {
         headers
-      }).then(body => body.json()).then(json => {
-        // console.log(json);
-        if (json) callback(json);
+      }).then(body => {
+        return body.json()
+      }).then(json => {
+        if (json) {
+          if (json.results.bindings.length > 0) {
+            for (var i = 0; i < json.results.bindings.length; i++) {
+
+              if (json.results.bindings[i].codcat !== undefined) {
+
+                if (json.results.bindings[i].codice_immatricolazione === undefined) {
+                  this.districts.push({
+                    district: `${json.results.bindings[i].label.value} (XX)`, //json.results.bindings[i].label.value,
+                    codImm: 'XX',
+                    codCat: json.results.bindings[i].codcat.value
+                  });
+                } else {
+                  this.districts.push({
+                    district: `${json.results.bindings[i].label.value} (${json.results.bindings[i].codice_immatricolazione.value})`, // json.results.bindings[i].label.value
+                    codImm: json.results.bindings[i].codice_immatricolazione.value,
+                    codCat: json.results.bindings[i].codcat.value
+                  });
+                }
+              }
+            }
+          }
+        }
       });
     },
     forceUpperCase(prop) {
